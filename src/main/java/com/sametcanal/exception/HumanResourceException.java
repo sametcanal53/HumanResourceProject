f1411213e6c9e5c9c362a02b1a77eed5bd59284d
Sametcan <sametcanal53@hotmail.com>
2022-09-15 14:56:41 +0300

    Custom Exception

--- src/main/java/com/sametcanal/exception/HumanResourceException.java
+++ src/main/java/com/sametcanal/exception/HumanResourceException.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class HumanResourceException extends RuntimeException{
+public class HumanResourceException extends RuntimeException {
     private String errorCode;
     private String errorMessage;
     private HttpStatus httpStatus;
